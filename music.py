c34c13a2835701da1799908156ebedca8bac918e
Matias Gjestvang Greaker <m-greaker@ti.com>
2024-02-29 18:11:16 +0100

    Adding draft changes

--- music.py
+++ music.py
@@ -3,33 +3,109 @@ import matplotlib.pyplot as plt
 
 
 
-def autocorrelation(x):
-    """Does the auto correlation of a signal X with no scaling"""
-    acorr = np.zeros(len(x))
-    x_per = np.concatenate([x,x])
-    for k in range(len(x)):
-        for n in range(len(x)):
-            # Out of range elements are considered to be zero
-            #if (n+k < len(x)):
-            acorr[k] = x[n]*x_per[n+k] + acorr[k]
-
+def autocorrelation(x, mode="periodic"):
+    """ Does the auto correlation of a signal X with no scaling
+        mode -- If 'periodic' x is assumed to repeat every N = length(x)
+                If 'zero' x is assumed zero out of its bound
+    """
+    N = len(x)
+    # Check if complex or not
+    if np.iscomplexobj(x):
+        comp = True
+        acorr = np.zeros(N)+1j*np.zeros(N)
+    else:
+        comp = False
+        acorr = np.zeros(N)
+    # Check mode
+    if (mode=="periodic"):
+        x_per = np.concatenate([x,x])
+    else:
+        x_per = np.concatenate([x,np.zeros(N)])
+    # Do autocorrelation
+    for k in range(N):
+        for n in range(N):
+            if comp:
+                acorr[k] = np.conjugate(x[n])*x_per[n+k] + acorr[k]
+            else:
+                acorr[k] = x[n]*x_per[n+k] + acorr[k]
     return acorr
 
-# Create a signal
+
+
+def show_weiner_khinchin():
+    # Create an arbitrary signal
+    start = 0
+    step = 0.001
+    Freq = 14
+    Freq2 = 50
+    N = 1000
+    t = np.arange(0,N)*step+start
+    # Create a complex signal
+    x = 20*np.exp(1j*2*np.pi*Freq*t) + 10*np.exp(1j*2*np.pi*Freq2*t)
+
+    ## Proof
+    # Calculate DFT of autocorrelation
+    r = autocorrelation(x)
+    r_fft = np.fft.fft(r)
+
+    # Calculate Power of the DFT
+    x_fft = np.fft.fft(x);
+    x_psd = np.abs(x_fft)**2
+
+    # Plot
+    plt.figure(1)
+    plt.title("|DFT(x)|^2 = DFT(r)")
+    plt.plot(x_psd, label='|DFT(x)|^2')
+    plt.plot(r_fft, label='DFT(r)')
+    plt.legend(loc="upper left")
+
+    plt.show()
+
+
+
+#show_weiner_khinchin()
+# 
 start = 0
 step = 0.001
-Freq = 10
-Freq2 = 20
-N = 1000
+Freq = 14
+Freq2 = 50
+N = 1000000
 t = np.arange(0,N)*step+start
-x = 20*np.sin(2*np.pi*Freq*t) + 10*np.sin(2*np.pi*Freq2*t)+np.random.normal(0,2,N)
+noise = np.random.normal(size=N)+1j*np.random.normal(size=N)
+x = 20*np.exp(1j*2*np.pi*Freq*t) + 10*np.exp(1j*2*np.pi*Freq2*t)
+
+
+sum = np.zeros(N+1)+1j*np.zeros(N+1)
+for i in range(N):
+    sum[i+1] = np.real(np.conjugate(noise[i]))*np.real(x[i]) + sum[i]
+
+
+plt.plot(np.abs(sum[1:-1])/N)
+print(sum[-1])
+plt.show()
+
+""" # Get autocorrelation
+y = autocorrelation(x)
+# Computing PSD
+y_psd = np.fft.fft(y)
+# Computing ESD
+y_esd = np.abs(np.fft.fft(x))**2
+
+print("Ratio if periodic: " + str(sum(y_psd/y_esd)/N))
 
 # Get autocorrelation
-y = autocorrelation(x)
+y = autocorrelation(x, mode='zero')
+# Computing PSD
+y_psd = np.fft.fft(y)
+# Computing ESD
+y_esd = np.abs(np.fft.fft(x))**2
+print("Ratio if zero: " + str(sum(y_psd/y_esd)/N))
 
+
+y = autocorrelation(x)
 plt.figure(0)
 plt.plot(x)
-plt.plot(y)
+plt.plot(y/N)
 # Computing PSD
 y_psd = np.fft.fft(y)
 # Computing ESD
@@ -43,3 +119,4 @@ plt.legend(loc="upper left")
 
 #plt.psd(x, Fs=1/step, NFFT=len(x))
 plt.show()
+ """
